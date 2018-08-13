598313cc506724cb87505ce165e21152bcf83cbf
kahyeelee@gmail.com <kahyeelee@gmail.com>
2018-08-13 05:44:20 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -12,6 +12,9 @@ function addElementToEndOfArray (array,element) {
   return array;
 }
 function destructivelyAddElementToEndOfArray (array,element) {
-  array.push(element)
-  return array
+  array.push(element);
+  return array;
+}
+function accessElementInArray (array,index) {
+  return 'element'[index];
 }
