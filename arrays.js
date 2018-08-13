daef614b3f87b7605b4f1eb40570e2245296370d
kahyeelee@gmail.com <kahyeelee@gmail.com>
2018-08-13 06:30:50 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,6 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 function addElementToBeginningOfArray (array,element) {
-  ['element',...array];
+  [element,...array];
   return array
 }
 function destructivelyAddElementToBeginningOfArray (array,element) {
@@ -8,7 +8,7 @@ function destructivelyAddElementToBeginningOfArray (array,element) {
   return array;
 }
 function addElementToEndOfArray (array,element) {
-  [...array, 'element'];
+  [...array, element];
   return array;
 }
 function destructivelyAddElementToEndOfArray (array,element) {
